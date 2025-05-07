38d68382ff4ec693c51e4d11460f4b0016a7247a
Juan Luis Faylon <4609140+jfaylon@users.noreply.github.com>
2025-05-07 16:06:32 +0800

    feat: version 1 of meme

---
+++ src/app/constants/index.ts
@@ -0,0 +1,14 @@
+export const UI_STRINGS = {
+  buttons: {
+    sendToTelegram: "Send to Telegram",
+    close: "Close",
+    generatePdf: "Generate PDF",
+  },
+  text: {
+    pdfPreview: "PDF Preview",
+    votes: "Votes",
+    memeTitle: "Meme Title",
+    memeImage: "Meme Image",
+    title: "Top 20 Memes of the Day",
+  },
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,199 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { UI_STRINGS } from "./constants";
 
-export default function Home() {
+interface Meme {
+  _id: string;
+  title: string;
+  images: {
+    source: {
+      url: string;
+    };
+  }[];
+  score: number;
+  url: string;
+  capturedAt: string;
+}
+
+const MemeList: React.FC = () => {
+  const [memes, setMemes] = useState<Meme[]>([]);
+  const [showModal, setShowModal] = useState(false);
+  const [pdfBlobUrl, setPdfBlobUrl] = useState<string | null>(null);
+  const [dates, setDates] = useState<string[]>([]);
+  const [selectedDate, setSelectedDate] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [isGeneratingPDF, setIsGeneratingPDF] = useState(false);
+  const [isSendingToTelegram, setIsSendingToTelegram] = useState(false);
+
+  useEffect(() => {
+    const fetchMemes = async () => {
+      setLoading(true);
+      try {
+        const memeResponse = await axios.get<Meme[]>(
+          `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/reddit/memes${selectedDate ? `?date=${selectedDate}` : ""}`,
+        );
+        setMemes(memeResponse.data);
+        const uniqueDatesResponse = await axios.get<string[]>(
+          `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/reddit/memes/dates`,
+        );
+        console.log(uniqueDatesResponse.data);
+        setDates(uniqueDatesResponse.data);
+        if (!selectedDate && uniqueDatesResponse.data.length > 0) {
+          setSelectedDate(uniqueDatesResponse.data[0]);
+        }
+      } catch (error) {
+        console.error("Error fetching memes:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchMemes();
+  }, [selectedDate]);
+
+  const handleGeneratePDF = async () => {
+    setIsGeneratingPDF(true);
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/reddit/memes/pdf?date=${selectedDate}`,
+        { responseType: "blob" },
+      );
+
+      const blob = new Blob([response.data], { type: "application/pdf" });
+      const blobUrl = URL.createObjectURL(blob);
+      setPdfBlobUrl(blobUrl);
+      setShowModal(true);
+      toast.success("PDF generated successfully!");
+    } catch (error) {
+      console.error("Error generating PDF:", error);
+      toast.error("Failed to generate PDF!");
+    } finally {
+      setIsGeneratingPDF(false);
+    }
+  };
+
+  const handleSendToTelegram = async () => {
+    setIsSendingToTelegram(true);
+    try {
+      await axios.post(
+        `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/reddit/memes/pdf/chatbot`,
+        { app: "telegram", date: selectedDate },
+      );
+      setShowModal(false);
+      toast.success("All memes sent to Telegram!");
+    } catch (error) {
+      console.error("Error sending memes to Telegram:", error);
+      toast.error("Failed to send memes to Telegram!");
+    } finally {
+      setIsSendingToTelegram(false);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setPdfBlobUrl(null);
+  };
+
+  const handleMemeRedirect = (url: string) => {
+    window.open(url, "_blank");
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="p-6">
+      <ToastContainer position="bottom-center" />
+      <h1 className="text-3xl font-bold mb-6">
+        {UI_STRINGS.text.title} - {selectedDate}
+      </h1>
+      <div className="flex space-x-4 mb-6">
+        <select
+          value={selectedDate || ""}
+          onChange={(e) => setSelectedDate(e.target.value)}
+          className="border border-gray-300 rounded px-4 py-2"
+        >
+          {dates.map((date) => (
+            <option key={date} value={date}>
+              {date}
+            </option>
+          ))}
+        </select>
+        <button
+          onClick={handleGeneratePDF}
+          disabled={isGeneratingPDF}
+          className={`bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 flex items-center ${
+            isGeneratingPDF ? "opacity-50 cursor-not-allowed" : ""
+          }`}
+        >
+          {isGeneratingPDF ? (
+            <div className="animate-spin rounded-full h-4 w-4 border-t-2 border-white mr-2"></div>
+          ) : null}
+          {UI_STRINGS.buttons.generatePdf}
+        </button>
+      </div>
+      {loading ? (
+        <div className="flex justify-center items-center h-64">
+          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"></div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ) : (
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {memes.map((meme, index) => (
+            <li
+              key={meme._id}
+              className="border rounded-lg shadow-md p-4 cursor-pointer hover:shadow-lg transition-shadow duration-300"
+              onClick={() => handleMemeRedirect(meme.url)}
+            >
+              <h3 className="text-lg font-semibold mb-2">
+                {index + 1}. {meme.title}
+              </h3>
+              <img
+                src={meme.images?.[0].source.url}
+                alt={meme.title}
+                className="max-w-full h-auto mb-4 rounded"
+              />
+              <p className="text-xl text-gray-700 mb-2">Votes: {meme.score}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {showModal && pdfBlobUrl && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+          <div className="bg-white rounded-lg p-6 w-[80%] h-[80%]">
+            <h2 className="text-xl font-bold mb-4">
+              {UI_STRINGS.text.pdfPreview}
+            </h2>
+            <iframe
+              src={pdfBlobUrl}
+              className="w-full h-[80%] border rounded mb-4"
+              title={UI_STRINGS.text.pdfPreview}
+            ></iframe>
+            <div className="flex space-x-4">
+              <button
+                onClick={handleSendToTelegram}
+                disabled={isSendingToTelegram}
+                className={`bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 flex items-center ${
+                  isSendingToTelegram ? "opacity-50 cursor-not-allowed" : ""
+                }`}
+              >
+                {isSendingToTelegram ? (
+                  <div className="animate-spin rounded-full h-4 w-4 border-t-2 border-white mr-2"></div>
+                ) : null}
+                {UI_STRINGS.buttons.sendToTelegram}
+              </button>
+              <button
+                onClick={handleCloseModal}
+                className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600"
+              >
+                {UI_STRINGS.buttons.close}
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
+
+export default MemeList;
